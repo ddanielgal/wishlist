3b84d505fdf6c120fd8ae2eab4218e0239c1ce48
den <danielgal.eu@danielgal.eu>
2025-11-22 11:35:17 +0100

    bigger image

--- src/components/Wishlist.tsx
+++ src/components/Wishlist.tsx
@@ -1,16 +1,30 @@
-import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import {
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from "./ui/item";
 
 export default function Wishlist() {
   return (
     <ol className="space-y-4">
-      <li>
-        <Card>
-          <CardHeader>
-            <CardTitle>Item title</CardTitle>
-            <CardDescription>Item Description.</CardDescription>
-          </CardHeader>
-        </Card>
-      </li>
+      <Item variant="outline">
+        <ItemMedia variant="image" className="size-36">
+          <img
+            src="https://placehold.co/144x144"
+            alt="Placeholder"
+            width={144}
+            height={144}
+            loading="lazy"
+            className="aspect-square object-cover"
+          />
+        </ItemMedia>
+        <ItemContent>
+          <ItemTitle>Item title</ItemTitle>
+          <ItemDescription>Item Description.</ItemDescription>
+        </ItemContent>
+      </Item>
     </ol>
   );
 }
