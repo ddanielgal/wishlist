f32bfdd8bb2976f56611465326eab4ec3ac7dcd9
den <danielgal.eu@danielgal.eu>
2025-11-29 10:58:56 +0100

    most of the stuff

--- src/components/Wishlist.tsx
+++ src/components/Wishlist.tsx
@@ -6,8 +6,8 @@ export default function Wishlist() {
       <Item variant="outline" role="listitem" className="items-start">
         <ItemMedia variant="image" className="size-36">
           <img
-            src="https://placehold.co/144x144"
-            alt="Placeholder"
+            src="https://s3-eu-west-1.amazonaws.com/images.linnlive.com/4026ef0cc7c4844b9d335306aa30fe5c/10518fb3-0456-4c38-96f3-cface3d52092.jpg"
+            alt="pulcsi"
             width={144}
             height={144}
             loading="lazy"
@@ -15,8 +15,152 @@ export default function Wishlist() {
           />
         </ItemMedia>
         <ItemContent className="gap-3">
-          <h3 className="text-lg leading-snug font-bold">Item title</h3>
-          <ItemDescription>Item Description.</ItemDescription>
+          <h3 className="text-lg leading-snug font-bold">Kapucnis pulcsi</h3>
+          <ItemDescription>
+            L méret. Cipzáros vagy nem cipzáros, mindegy. Mindegy milyen színű.
+            Fekete, piros és kék már van. Ne legyen rajta semmi. Kényelmes,
+            igazi anyagból.
+          </ItemDescription>
+        </ItemContent>
+      </Item>
+
+      <Item variant="outline" role="listitem" className="items-start">
+        <ItemMedia variant="image" className="size-36">
+          <img
+            src="https://i5.walmartimages.com/seo/Oral-B-iO-Gentle-Care-Replacement-Heads-Electric-Toothbrush-Brush-Heads-Black-2-Count_56beb798-6c16-4834-ae06-6b151d449c8f.674ba7b5141654747fc21d9624ca08ba.jpeg"
+            alt="fogkefe fej"
+            width={144}
+            height={144}
+            loading="lazy"
+            className="aspect-square object-cover"
+          />
+        </ItemMedia>
+        <ItemContent className="gap-3">
+          <h3 className="text-lg leading-snug font-bold">Fogkefe fej</h3>
+          <ItemDescription>
+            Oral-B iO 4 fogkeféhez. "Gentle Care" típus preferált. Elég egy 4-es
+            csomag és majd jövő karácsonyra is kérek még.
+          </ItemDescription>
+        </ItemContent>
+      </Item>
+
+      <Item variant="outline" role="listitem" className="items-start">
+        <ItemMedia variant="image" className="size-36">
+          <img
+            src="https://branthorn.hu/cdn/shop/files/gempages_561171690445341786-eae957e7-993c-4509-bd0e-307118dc7495.gif?v=500136139664623077"
+            alt="kés élező"
+            width={144}
+            height={144}
+            loading="lazy"
+            className="aspect-square object-cover"
+          />
+        </ItemMedia>
+        <ItemContent className="gap-3">
+          <h3 className="text-lg leading-snug font-bold">Gurigázó kés élező</h3>
+          <ItemDescription>
+            Branthorn, de más márka is jó. Valószínűleg ezt is az aliexpressről
+            dropshippelik.{" "}
+            <a href="https://branthorn.hu/" target="_blank">
+              Link.
+            </a>
+          </ItemDescription>
+        </ItemContent>
+      </Item>
+
+      <Item variant="outline" role="listitem" className="items-start">
+        <ItemMedia variant="image" className="size-36">
+          <img
+            src="https://www.watchgecko.com/cdn/shop/products/watch-straps-vintage-highley-genuine-leather-watch-strap-reddish-brown-34686494736547_1066x.jpg?v=1661996709"
+            alt="óraszíj"
+            width={144}
+            height={144}
+            loading="lazy"
+            className="aspect-square object-cover"
+          />
+        </ItemMedia>
+        <ItemContent className="gap-3">
+          <h3 className="text-lg leading-snug font-bold">Óraszíj</h3>
+          <ItemDescription>
+            Igazi bőr, barna bőr (mint a cipőm), fekete bőr, fekete fém is
+            lehet.
+          </ItemDescription>
+        </ItemContent>
+      </Item>
+
+      <Item variant="outline" role="listitem" className="items-start">
+        <ItemMedia variant="image" className="size-36">
+          <img
+            src="https://www.engguidebook.com/assets/images/EngGuidebook_cover.png"
+            alt="the software engineer's guidebook"
+            width={144}
+            height={144}
+            loading="lazy"
+            className="aspect-square object-cover"
+          />
+        </ItemMedia>
+        <ItemContent className="gap-3">
+          <h3 className="text-lg leading-snug font-bold">
+            Orosz Gergely: The Software Engineer's Guidebook
+          </h3>
+          <ItemDescription>
+            Könyv.{" "}
+            <a href="https://www.engguidebook.com/" target="_blank">
+              Link.
+            </a>
+          </ItemDescription>
+        </ItemContent>
+      </Item>
+
+      <Item variant="outline" role="listitem" className="items-start">
+        <ItemMedia variant="image" className="size-36">
+          <img
+            src="https://i.pinimg.com/originals/11/b2/dc/11b2dc9f56df933dd3f5bf790d9e3409.jpg"
+            alt="tepláky"
+            width={144}
+            height={144}
+            loading="lazy"
+            className="aspect-square object-cover"
+          />
+        </ItemMedia>
+        <ItemContent className="gap-3">
+          <h3 className="text-lg leading-snug font-bold">Kinti tepláky</h3>
+          <ItemDescription>
+            L méret. Cipzáros zsebű, kint hordható. Mindegy milyen színű. Fekete
+            már van. Kényelmes, igazi anyagból.
+          </ItemDescription>
+        </ItemContent>
+      </Item>
+
+      <Item variant="outline" role="listitem" className="items-start">
+        <ItemMedia variant="image" className="size-36">
+          <img
+            src="https://ae01.alicdn.com/kf/H6db7d685cc23448ba94d2f11ca168728z/Men-s-Autumn-Summer-Socks-Classic-Patchwork-Mesh-Men-Short-Socks-Deodorant-Male-Sock-Casual-slippers.jpg"
+            alt="zoknik"
+            width={144}
+            height={144}
+            loading="lazy"
+            className="aspect-square object-cover"
+          />
+        </ItemMedia>
+        <ItemContent className="gap-3">
+          <h3 className="text-lg leading-snug font-bold">Rövid szárú zoknik</h3>
+        </ItemContent>
+      </Item>
+
+      <Item variant="outline" role="listitem" className="items-start">
+        <ItemMedia variant="image" className="size-36">
+          <img
+            src="https://www.siatex.com/wp-content/uploads/2023/02/Mens-Underwear-Boxer-Briefs.jpg.webp"
+            alt="boxerek"
+            width={144}
+            height={144}
+            loading="lazy"
+            className="aspect-square object-cover"
+          />
+        </ItemMedia>
+        <ItemContent className="gap-3">
+          <h3 className="text-lg leading-snug font-bold">Boxer alsók</h3>
+          <ItemDescription>L méret. Kényelmes, igazi anyagból.</ItemDescription>
         </ItemContent>
       </Item>
     </ol>
